6eddfdd929c8f386ac41769ce8cedea1ed19b5f0
voruti <45130096+voruti@users.noreply.github.com>
2023-12-08 21:41:06 +0100

    Add functions to util

--- src/util.ts
+++ src/util.ts
@@ -2,6 +2,35 @@
 
 const baseHeight: number = 640; // TODO: replace with dynamically
 
+export function bytesToHex(bytes: Uint8Array): string {
+    return Array.from(bytes)
+        .map((byte) => byte.toString(16).padStart(2, "0"))
+        .join(" ");
+}
+
+export function hexToBytes(hex: string): Uint8Array {
+    const hexWithoutSpaces = hex.replace(/\s/g, "");
+    const byteCount = hexWithoutSpaces.length / 2;
+    const byteArray = new Uint8Array(byteCount);
+
+    for (let i = 0; i < byteCount; ++i) {
+        byteArray[i] = parseInt(
+            hexWithoutSpaces.substring(i * 2, (i + 1) * 2),
+            16
+        );
+    }
+
+    return byteArray;
+}
+
+export function bytesToInt(bytes: Uint8Array): number {
+    return bytes.reduceRight((acc, byte) => (acc << 8) | byte, 0);
+}
+
+export function bytesToUtf8(bytes: Uint8Array): string {
+    return new TextDecoder().decode(bytes);
+}
+
 export class Height {
     private _value: number;
 
