b74199fab22acf718a94e3d4ff0b8a8b1e868919
voruti <45130096+voruti@users.noreply.github.com>
2023-12-10 00:13:37 +0100

    Fix?

--- src/gatt.ts
+++ src/gatt.ts
@@ -131,7 +131,7 @@ export class ReferenceOutputService extends Service {
     static decodeHeightSpeed(buffer: Buffer,config:Config): HeightAndSpeed {
         console.log("decodeHeightSpeed entering with",buffer);
 
-         const dataView  = new DataView(buffer.buffer);
+         const dataView  = new DataView(buffer.buffer, 0, 4);
          console.log("dataView",dataView);
          const height: number = dataView.getUint16(0, true);
         const speed: number = dataView.getInt16(2, true);
