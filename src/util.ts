e16bc0db1d8ce994b31ddc753c581bff7364fa25
voruti <45130096+voruti@users.noreply.github.com>
2023-12-10 20:45:21 +0100

    Value classes

--- src/util.ts
+++ src/util.ts
@@ -106,73 +106,55 @@ export function makeIter<T>() {
     return [get(), put];
 }
 
-export class Height {
-    private _value: number;
+abstract class Value {
+    protected _value: number;
 
+    constructor(value: number) {
+        this._value = value;
+    }
+
+    public get value(): number {
+        return this._value;
+    }
+
+    public abstract get human(): number;
+}
+
+/**
+ * Height#_value is relative height (ie. height above base height of desk) in 10ths of a mm
+ */
+export class Height extends Value {
     constructor(
         height: number,
         private config: Config,
         convertFromHuman: boolean = false
     ) {
         debugLog(config, "Height#init enter", height, convertFromHuman);
-
-        if (convertFromHuman) {
-            this._value = Height.heightToInternalHeight(height, config);
-        } else {
-            this._value = height; // relative height in 10ths of a mm
-        }
-
+        super(convertFromHuman ? (height - config.baseHeight) * 10 : height);
         debugLog(config, "Height#init end", this._value);
     }
 
-    public get value(): number {
-        return this._value;
-    }
-
-    public get human(): number {
-        return Height.internalHeightToHeight(this.value, this.config);
-    }
-
-    public static heightToInternalHeight(
-        height: number,
-        config: Config
-    ): number {
-        return (height - config.baseHeight) * 10;
-    }
-
-    public static internalHeightToHeight(
-        height: number,
-        config: Config
-    ): number {
-        return height / 10 + config.baseHeight;
+    /**
+     * @returns height above floor in mm
+     */
+    public override get human(): number {
+        return this.value / 10 + this.config.baseHeight;
     }
 }
 
-export class Speed {
-    private _value: number;
-
-    constructor(speed: number, convert: boolean = false) {
-        if (convert) {
-            this._value = Speed.speedToInternalSpeed(speed);
-        } else {
-            this._value = speed; // speed in 100ths of a mm/s
-        }
+/**
+ * Speed#_value is speed in 100ths of a mm/s
+ */
+export class Speed extends Value {
+    constructor(speed: number, convertFromHuman: boolean = false) {
+        super(convertFromHuman ? speed * 100 : speed);
     }
 
-    public get value(): number {
-        return this._value;
-    }
-
-    public get human(): number {
-        return Speed.internalSpeedToSpeed(this.value);
-    }
-
-    public static speedToInternalSpeed(speed: number): number {
-        return speed * 100;
-    }
-
-    public static internalSpeedToSpeed(speed: number): number {
-        return speed / 100;
+    /**
+     * @returns speed in mm/s
+     */
+    public override get human(): number {
+        return this.value / 100;
     }
 }
 
