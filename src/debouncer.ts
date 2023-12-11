831d0a8512e76f95ad7cc0cbf0ea4951381ce2da
voruti <45130096+voruti@users.noreply.github.com>
2023-12-11 19:47:39 +0100

    Test simplify debounce

--- src/debouncer.ts
+++ src/debouncer.ts
@@ -1,15 +1,15 @@
-import { setTimeout } from "timers/promises";
-
 const debounceRunsMap = new Map<string, string>();
 
 export async function debounce(
     identifier: string,
     timeoutMs: number
-): Promise<boolean> {
+): Promise<void> {
     const thisRun: string = Math.random().toString(36).substring(2, 9);
     debounceRunsMap.set(identifier, thisRun);
 
-    return setTimeout(timeoutMs).then(
-        () => debounceRunsMap.get(identifier) === thisRun
-    );
+    return new Promise<void>((resolve, reject) => {
+        setTimeout(() => {
+            debounceRunsMap.get(identifier) === thisRun ? resolve() : reject();
+        }, timeoutMs);
+    });
 }
